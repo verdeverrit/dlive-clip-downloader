3c3837fd3c3e2737309d92f369277711fa07cd32
verrit <67809872+verdeverrit@users.noreply.github.com>
2020-07-08 12:58:14 +1000

    Add files via upload

--- dclip.sh
+++ dclip.sh
@@ -6,6 +6,9 @@ done
 for i in ${links[@]}; do
 	l=$(echo $i | sed 's/.*\///g')
 	echo "FETCHING $l"
-	curl --silent $i | sed 's/</\n/g' | grep mp4 | head -n1 | sed 's/^.*https/https/;s/".*//' | xargs curl --progress-bar -O
+	link=$(curl --silent $i | sed 's/</\n/g' | grep mp4 | head -n1 | sed 's/^.*https/https/;s/".*//')
+	filename=$(echo $link | sed 's/.*\///g')
+	curl --progress-bar -O $link
+	echo "SAVED AS $filename"
 done
 echo "DONE"
