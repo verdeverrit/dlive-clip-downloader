2a82f59fb3632ee2f86f3d69be11cd209384b271
verrit <67809872+verdeverrit@users.noreply.github.com>
2020-07-08 12:29:44 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+This script downloads clips from dlive, because youtube-dl only likes dlive's vods and streams. Accepts multiple urls.
+
+Usage: ./dclip.sh <url> <url>
---
+++ dclip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+links=()
+for a in "$*"; do 
+	links+=($a)
+done
+for i in ${links[@]}; do
+	l=$(echo $i | sed 's/.*\///g')
+	echo "FETCHING $l"
+	curl --silent $i | sed 's/</\n/g' | grep mp4 | head -n1 | sed 's/^.*https/https/;s/".*//' | xargs curl --progress-bar -O
+	echo "DONE"
+done
